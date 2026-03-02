08dadddbf8cd1bf6084ea05cb3a8c37f8c231f61
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-03-01 21:53:07 -0300

    adicionando o Validation @FUTURE na validade para nao recebe datas que ja passaram

--- src/main/java/com/estudando/spring/dtos/RemedioDTOCreate.java
+++ src/main/java/com/estudando/spring/dtos/RemedioDTOCreate.java
@@ -2,6 +2,7 @@ package com.estudando.spring.dtos;
 
 import com.estudando.spring.entity.Laboratorio;
 import com.estudando.spring.entity.Via;
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -20,6 +21,7 @@ public record RemedioDTOCreate(
         String lote,
 
         @NotNull (message = "A validade do remédio é obrigatória")
+        @Future
         LocalDate validade,
 
         @Min(value = 1, message = "A quantidade do remédio deve ser pelo menos 1")
