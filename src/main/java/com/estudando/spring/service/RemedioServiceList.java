bb7df461b0b52a639e59c136f755e9a5ea5354be
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-03-05 18:41:35 -0300

    criando metodo de busca de remedios por via na camada service

--- src/main/java/com/estudando/spring/service/RemedioServiceList.java
+++ src/main/java/com/estudando/spring/service/RemedioServiceList.java
@@ -1,6 +1,7 @@
 package com.estudando.spring.service;
 
 import com.estudando.spring.dtos.RemedioResponseDTO;
+import com.estudando.spring.entity.Via;
 import com.estudando.spring.repository.RemedioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -33,6 +34,20 @@ public class RemedioServiceList {
 
     public List<RemedioResponseDTO> buscarRemediosPorNome(String nome) {
         return repository.findByNomeContainingIgnoreCase(nome).stream()
+                .map(remedio -> new RemedioResponseDTO(
+                        remedio.getId(),
+                        remedio.getNome(),
+                        remedio.getVia(),
+                        remedio.getLote(),
+                        remedio.getValidade(),
+                        remedio.getQuantidade(),
+                        remedio.getLaboratorio()
+                ))
+                .toList();
+    }
+
+    public  List<RemedioResponseDTO> buscarRemediosPorVia(Via via) {
+        return repository.findByVia(via).stream()
                 .map(remedio -> new RemedioResponseDTO(
                         remedio.getId(),
                         remedio.getNome(),
