7e8db8fc76e906805cb5e61893762a503251dbd9
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2026-03-05 19:42:29 -0300

    ajustando problema de repetição de código e criando método de conversão

--- src/main/java/com/estudando/spring/service/RemedioServiceList.java
+++ src/main/java/com/estudando/spring/service/RemedioServiceList.java
@@ -2,9 +2,9 @@ package com.estudando.spring.service;
 
 import com.estudando.spring.dtos.RemedioResponseDTO;
 import com.estudando.spring.entity.Laboratorio;
+import com.estudando.spring.entity.RemedioModel;
 import com.estudando.spring.entity.Via;
 import com.estudando.spring.repository.RemedioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,7 +12,6 @@ import java.util.List;
 @Service
 public class RemedioServiceList {
 
-    @Autowired
     private final RemedioRepository repository;
 
     public RemedioServiceList(RemedioRepository repository) {
@@ -20,58 +19,42 @@ public class RemedioServiceList {
     }
 
     public List<RemedioResponseDTO> listarRemedios() {
-        return repository.findAll().stream()
-                .map(remedio -> new RemedioResponseDTO(
-                        remedio.getId(),
-                        remedio.getNome(),
-                        remedio.getVia(),
-                        remedio.getLote(),
-                        remedio.getValidade(),
-                        remedio.getQuantidade(),
-                        remedio.getLaboratorio()
-                ))
+        return repository.findAll()
+                .stream()
+                .map(this::toDTO)
                 .toList();
     }
 
     public List<RemedioResponseDTO> buscarRemediosPorNome(String nome) {
-        return repository.findByNomeContainingIgnoreCase(nome).stream()
-                .map(remedio -> new RemedioResponseDTO(
-                        remedio.getId(),
-                        remedio.getNome(),
-                        remedio.getVia(),
-                        remedio.getLote(),
-                        remedio.getValidade(),
-                        remedio.getQuantidade(),
-                        remedio.getLaboratorio()
-                ))
+        return repository.findByNomeContainingIgnoreCase(nome)
+                .stream()
+                .map(this::toDTO)
                 .toList();
     }
 
-    public  List<RemedioResponseDTO> buscarRemediosPorVia(Via via) {
-        return repository.findByVia(via).stream()
-                .map(remedio -> new RemedioResponseDTO(
-                        remedio.getId(),
-                        remedio.getNome(),
-                        remedio.getVia(),
-                        remedio.getLote(),
-                        remedio.getValidade(),
-                        remedio.getQuantidade(),
-                        remedio.getLaboratorio()
-                ))
+    public List<RemedioResponseDTO> buscarRemediosPorVia(Via via) {
+        return repository.findByVia(via)
+                .stream()
+                .map(this::toDTO)
                 .toList();
     }
 
     public List<RemedioResponseDTO> buscarRemediosPorLaboratorio(Laboratorio laboratorio) {
-        return repository.findByLaboratorio(laboratorio).stream()
-                .map(remedio -> new RemedioResponseDTO(
-                        remedio.getId(),
-                        remedio.getNome(),
-                        remedio.getVia(),
-                        remedio.getLote(),
-                        remedio.getValidade(),
-                        remedio.getQuantidade(),
-                        remedio.getLaboratorio()
-                ))
+        return repository.findByLaboratorio(laboratorio)
+                .stream()
+                .map(this::toDTO)
                 .toList();
+    }
+
+    private RemedioResponseDTO toDTO(RemedioModel remedio) {
+        return new RemedioResponseDTO(
+                remedio.getId(),
+                remedio.getNome(),
+                remedio.getVia(),
+                remedio.getLote(),
+                remedio.getValidade(),
+                remedio.getQuantidade(),
+                remedio.getLaboratorio()
+        );
     }
 }
